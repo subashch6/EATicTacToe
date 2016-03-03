878432bcc35951028b187964e2438cbc3337b4d1
jeffreyak <kreindlerjeffrey@gmail.com>
2016-03-03 09:03:35 -0500

    Added files via upload

--- driveCode.c
+++ driveCode.c
@@ -1,6 +1,6 @@
 #pragma config(Sensor, S1,     Gyro,           sensorEV3_Gyro)
 #pragma config(Sensor, S2,     LineTracker1,   sensorEV3_Color)
-#pragma config(Sensor, S3,     LineTraker2,    sensorEV3_Color)
+#pragma config(Sensor, S3,     LineTracker2,   sensorNone)
 #pragma config(Motor,  motorA,          Left,          tmotorEV3_Large, PIDControl, driveLeft, encoder)
 #pragma config(Motor,  motorB,          Right,         tmotorEV3_Large, PIDControl, driveRight, encoder)
 #pragma config(Motor,  motorC,          DropMotor,     tmotorEV3_Large, PIDControl, encoder)
@@ -8,47 +8,46 @@
 
 task drop()
 {
-wait(500);
-setMotorSpeed(motorC,25);
+	delay(500);
+	setMotorSpeed(motorC,25);
 }
 task rightTurnAfterLine()
 {
-	reset(S1);
-	if(getColorReflected(S2) & getColorReflected(S3) == 'white')
+	resetGyro(Gyro);
+	if(getColorReflected(LineTracker1) == 256 && getColorReflected(LineTracker2) == 256)
 	{
-		while(getGyroDegrees(S1)<90)
+		while(getGyroDegrees(Gyro)<90)
 		{
-			setMotorSpeed(motorA,50);
-			setMotorSpeed(motorB,-50);
+			setMotorSpeed(Left,50);
+			setMotorSpeed(Right,-50);
 		}
 	}
 
 }
 task leftTurnAfterLine()
 {
-	reset(S1);
-	if(getColorReflected(S2) & getColorReflected(S3) == 'white')
+	if(getColorReflected(LineTracker1)== 256 & getColorReflected(LineTracker2) == 256)
 	{
 		while(getGyroDegrees(S1)<-90)
 		{
-			setMotorSpeed(motorA,-50);
-			setMotorSpeed(motorB,50);
+			setMotorSpeed(Left,-50);
+			setMotorSpeed(Right,50);
 		}
 	}
 task leftTurn()
 {
-	while(getGyroDegrees(S1)<-90)
+	while(getGyroDegrees(Gyro)<-90)
 		{
-			setMotorSpeed(motorA,-50);
-			setMotorSpeed(motorB,50);
+			setMotorSpeed(Left,-50);
+			setMotorSpeed(Right,50);
 		}
 }
 task rightTurn()
 {
-while(getGyroDegrees(S1)<90)
+while(getGyroDegrees(Gyro)<90)
 		{
-			setMotorSpeed(motorA,50);
-			setMotorSpeed(motorB,-50);
+			setMotorSpeed(Left,50);
+			setMotorSpeed(Right,-50);
 		}
 }
 
@@ -58,7 +57,7 @@ lineTrackLeft(LineTracker1,50,50,100);
 }
 task BottomLeft()
 {
-	wait(1000);
+	delay(1000);
 	forward();
 }
 task BottomMiddle()
@@ -71,7 +70,7 @@ task BottomRight()
 }
 task middleLeft()
 {
-	wait(2000);
+	delay(2000);
 	forward();
 }
 task middleMiddle()
