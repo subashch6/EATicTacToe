e1af4ed2f59f0789dc252ea2430519a6c5d9cefc
jkhoylman <jkhoylman@gmail.com>
2016-03-03 08:57:03 -0500

    Started work on choosing algorithm

--- workingCode1.c
+++ workingCode1.c
@@ -10,7 +10,7 @@ int X = 1;
 int O = 4;
 int empty = 0;
 int board[3][3] = {{empty, empty, empty}, {empty, empty, empty}, {empty, empty, empty}};
-
+int move[2] = {0,0};
 /**
 * This function checks to see if a particular array has a winning combo.
 *
@@ -57,6 +57,81 @@ bool checkBoardForWin()
 	return (hasWon(row1) || hasWon(row2) || hasWon(row3) || hasWon(column1) || hasWon(column2) || hasWon(column3) || hasWon(diagonal1) || hasWon(diagonal2));
 }
 
+bool checkToWin()
+{
+	int row1[3] = {board[0][0], board[0][1], board[0][2]};
+	int row2[3] = {board[1][0], board[1][1], board[1][2]};
+	int row3[3] = {board[2][0], board[2][1], board[2][2]};
+
+	int column1[3] = {board[0][0], board[1][0], board[2][0]};
+	int column2[3] = {board[0][1], board[1][1], board[2][1]};
+	int column3[3] = {board[0][2], board[1][2], board[2][2]};
+
+	int diagonal1[3] = {board[0][0], board[1][1], board[2][2]};
+	int diagonal2[3] = {board[0][2], board[1][1], board[2][0]};
+
+	if(numMoves %2 == 0)
+	{
+		if(row1[0]+row1[1]+row1[2] == X+X)
+		{
+			if(row1[0] == empty)
+			{
+				move[0] = 0;
+				move[1] = 0;
+			}
+			else if(row1[1] == empty)
+			{
+				move[0] = 0;
+				move[1] = 1;
+			}
+			else
+			{
+
+			}
+		}
+		else if(row2[0]+row2[1]+row2[2] == X+X)
+		{
+
+		}
+		else if(row3[0]+row3[1]+row3[2] == X+X)
+		{
+
+		}
+		else if(column1[0]+column1[1]+column1[2] == X+X)
+		{
+
+		}
+		else if(column2[0]+column2[1]+column2[2] == X+X)
+		{
+
+		}
+		else if(column3[0]+column3[1]+column3[2] == X+X)
+		{
+
+		}
+		else if(diagonal1[0]+diagonal1[1]+diagonal1[2] == X+X)
+		{
+
+		}
+		else if(diagonal2[0]+diagonal2[1]+diagonal2[2] == X+X)
+		{
+
+		}
+
+	}
+	else
+	{
+
+	}
+
+	return false;
+}
+
+bool checkToBlockWin()
+{
+
+}
+
 /**
 * Checks to see if the target is empty or not.
 *
@@ -80,13 +155,16 @@ bool isSpaceEmpty(int positionX, int positionY)
 * move[1] = yPosition
 */
 
-int* chooseMove()
+void chooseMove()
 {
-	int move[2] = {0,0};
+	if(numMoves%2==0)//X moves
+	{
 
-	//Code to fill move
+	}
+	else //O moves
+	{
 
-	return move;
+	}
 }
 
 /**
@@ -141,13 +219,13 @@ task main()
 	sprintf(buffer,"%d %d",*myMove,*(myMove+1));
 	displayString(10,buffer);
 
-	if(numMoves%2)
+	if(numMoves%2==0)
 	{
-		addPiece(O,myMove[0],myMove[1]); //Player 2 places O
+		addPiece(X,myMove[0],myMove[1]); //Player 2 places O
 	}
 	else
 	{
-		addPiece(X,0,0); //Player 1 places X
+		addPiece(O,0,0); //Player 1 places X
 	}
 	checkBoardForWin();
 	while(true)
